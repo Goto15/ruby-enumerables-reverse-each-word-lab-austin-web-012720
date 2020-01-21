b6b08ebf369b1cf8afd8c1e4bb634fbf4d6a1080
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 03:08:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(string)
   string_arr = string.split(" ")
   
-  string_arr.collect {
-  for i in 0...string_arr.length
-    string_arr[i].reverse!
-  end
+  string_arr.collect { |x| x.reverse! }
   string_arr = string_arr.join(" ")
   
   return string_arr
